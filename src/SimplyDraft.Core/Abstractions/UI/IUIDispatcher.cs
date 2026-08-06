602040c8d435196461570313f368b2f0b3f1c270
JM <jingming.tan12@gmail.com>
2026-08-07 07:17:31 +0800

    Delete src/SimplyDraft.Core directory

--- src/SimplyDraft.Core/Abstractions/UI/IUIDispatcher.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Tan Jing Ming. Use of this software is governed by LICENSE.md.
-
-namespace SimplyDraft.Core.Abstractions.UI;
-
-public interface IUIDispatcher
-{
-    bool IsOnUIThread {get;}
-    void Post(Action action);
-}
--- src/SimplyDraft.Core/Domains/Documents/TemplateDocument.cs
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Tan Jing Ming. Use of this software is governed by LICENSE.md.
-
-namespace SimplyDraft.Core.Domains.Documents;
-
-public sealed class TemplateDocument : LibraryDocument
-{
-}
