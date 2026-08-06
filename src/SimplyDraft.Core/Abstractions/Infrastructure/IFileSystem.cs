486cf3a3a0fdc2adfc3f068f00471c47efa89b7a
JM <jingming.tan12@gmail.com>
2026-08-06 22:59:30 +0800

    Update IFileSystem.cs

--- src/SimplyDraft.Core/Abstractions/Infrastructure/IFileSystem.cs
+++ src/SimplyDraft.Core/Abstractions/Infrastructure/IFileSystem.cs
@@ -6,6 +6,4 @@ public interface IFileSystem
 {
     Task<string> ReadAllTextAsync(string path, CancellationToken ct);
     Task WriteAllTextAsync(string path, string text, CancellationToken ct);
-    bool FileExists(string path);
-    void CreateDirectory(string path);
 }
