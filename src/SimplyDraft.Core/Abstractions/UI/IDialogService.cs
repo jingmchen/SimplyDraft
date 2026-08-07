1f9f46d3e42fb9d64afa794568eca782917997a0
JM <jingming.tan12@gmail.com>
2026-08-07 11:49:08 +0000

    edit

--- src/SimplyDraft.Core/Abstractions/UI/IDialogService.cs
+++ src/SimplyDraft.Core/Abstractions/UI/IDialogService.cs
@@ -1,6 +1,5 @@
 // Copyright (c) Tan Jing Ming. Use of this software is governed by LICENSE.md.
 
-using SimplyDraft.Core.Domains.Documents;
 using SimplyDraft.Core.Domains.Library;
 using SimplyDraft.Core.Domains.UI;
 
@@ -8,15 +7,9 @@ namespace SimplyDraft.Core.Abstractions.UI;
 
 public interface IDialogService
 {
-    void OpenEditor(LibraryItem item);
-    Task<bool> OpenSettingsAsync();
-
     Task<string?> PromptAsync(string title, string prompt, string initial = "");
     Task<int> ChooseAsync(string title, string message, params string[] buttons);
-    Task OpenBatchAsync(TemplateDocument tmplate);
-
     Task<LibraryItem?> PickTemplateAsync(IEnumerable<LibraryItem> templates);
     Task<NewTemplateSelection?> OpenNewTemplateAsync(IReadOnlyList<string> seedTemplateNames);
-    Task<string?> OpenGenerateChildAsync(TemplateDocument tmplate, string childName);
     Task<IReadOnlyList<VariableDeclaration>?> EditVariablesAsync(IReadOnlyList<VariableDeclaration> current);
 }
