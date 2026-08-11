74736ff8d627fa56a2c9636a5680606707a0d23f
JM <jingming.tan12@gmail.com>
2026-08-11 19:52:04 +0800

    Update LoggingSettings.cs
    Remove dependencies in core

--- src/SimplyDraft.Core/Configuration/LoggingSettings.cs
+++ src/SimplyDraft.Core/Configuration/LoggingSettings.cs
@@ -1,11 +1,11 @@
 // Copyright (c) Tan Jing Ming. Use of this software is governed by LICENSE.md.
 
-using Serilog.Events;
+using Microsoft.Extensions.Logging
 
 namespace SimplyDraft.Core.Configuration;
 
 public sealed record LoggingSettings
 {
-    public LogEventLevel MinimumLevel {get; set;} = LogEventLevel.Information;
+    public LogLevel MinimumLevel {get; set;} = LogLevel.Information;
     public int RetainedFileCountLimit {get; set;} = 7;
 }
